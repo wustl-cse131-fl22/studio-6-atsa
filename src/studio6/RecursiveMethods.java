859efd741d68d84e4542283894f06329517dbe8b
adamshumway <adamshumway@localhost>
2022-10-26 17:14:23 -0500

    Studio 6 Alex Adam Toby Sebastion

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -14,7 +14,13 @@ public class RecursiveMethods {
 	public static double geometricSum(int n) {
 		
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
+		if(n ==0) {	
+		return 0;
+		}
+		else {
+			return (1/(Math.pow(2, n))) + geometricSum(n-1);
+			
+		}
 		
 	}
 
@@ -29,7 +35,20 @@ public class RecursiveMethods {
 	public static int gcd(int p, int q) {
 		
 			// FIXME compute the gcd of p and q using recursion
-			return 0;
+			if(p%q ==0) {
+				return q;
+			}
+			else {
+				int x = p%q;
+			int gcdValue = gcd(q, x);
+			
+			return gcdValue;
+			}
+			
+		
+		
+		
+			
 		
 	}
 
@@ -42,11 +61,28 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+		return helper(array, 0, array.length-1);
 		
 	}
+		
+			// FIXME create a helper method that can recursively reverse the given array
+	public static int[] helper(int [] reverse, int startIndex, int endIndex) {
+		
+			
+			if(startIndex >= reverse.length/2) {
+				return reverse;
+			}
+			else {
+					int temp = reverse[startIndex];
+					reverse[startIndex] = reverse[endIndex];
+					reverse[endIndex] = temp;
+					
+			return helper(reverse, startIndex+1, endIndex-1);
+				}
+	}
+
+		
+	
 
 	/**
 	 * @param xCenter                       x-coordinate of the center of the circle
